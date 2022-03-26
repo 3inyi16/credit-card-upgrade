411c8ab1301ea7c1677e7fbc2f4eaed93fa71fb5
3inyi16 <101925160+3inyi16@users.noreply.github.com>
2022-03-26 16:09:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,12 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
+        purchases = request.form.get("purchases")
+        suppcard = request.form.get("suppcard")
+        print(purchases, suppcard)
         return(render_template("index.html", result1="1", result2="1", result3="1"))
-
     else:
         return(render_template("index.html", result1="2", result2="2", result3='2'))
 
-
 if __name__ == "__main__":
     app.run()
-
-#http://127.0.0.1:5000
